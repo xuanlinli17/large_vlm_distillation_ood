cf3a4ece1f611a0112de93a5b09202bd349435c1
Seerkfang <895290575@qq.com>
2023-07-04 11:46:12 -0700

    add data data_preprocess.sh

---
+++ scripts/data_preprocess.sh
@@ -0,0 +1,18 @@
+## CaltechBirds
+wget https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz?download=1
+
+## FLower102
+wget https://www.robots.ox.ac.uk/~vgg/data/flowers/102/102flowers.tgz 
+wget https://www.robots.ox.ac.uk/~vgg/data/flowers/102/imagelabels.mat
+
+## Food101
+wget http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz
+
+## SUN397
+wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
+
+## StanfordCars https://www.kaggle.com/datasets/jessicali9530/stanford-cars-dataset?resource=download-directory
+## Log in kaggle and download the cars_train directory.
+
+## tiered-ImageNet https://lyy.mpi-inf.mpg.de/mtl/download/
+## Click "Agree to the aforementioned terms.", then download tiered-imagenet.
