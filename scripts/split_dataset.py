cb7497af2030b486deda294225606644a280384f
Seerking <895290575@qq.com>
2023-07-05 09:58:41 -0700

    Update.

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -12,14 +12,14 @@ args = parser.parse_args()
 
 dataset_prefix = {"CaltechBirds": "CUB_200_2011/images",
                   "Flower102": "jpg",
-                  "Food101": "",
-                  "StanfordCars": "",
+                  "Food101": "images",
+                  "StanfordCars": "cars_train",
                   "SUN397": "",
-                  "tiered-ImageNet": ""}
+                  "tiered-ImageNet": "tiered_imagenet/train"}
 
 dataset_root = Path(args.data_root)
 if not dataset_root.exists():
-    raise ValueError(f"Dataset {dataset} not found in {data_root}")
+    raise ValueError(f"Dataset {args.dataset_name} not found in {args.data_root}")
 print(f"Processing {args.dataset_name} from {args.data_root}")
 for split in ["train", "val", "val_on_train"]:
     split_root = dataset_root / split
@@ -38,7 +38,11 @@ for split in ["train", "val", "val_on_train"]:
         class_dir = split_root / class_name
         if not class_dir.exists():
             os.mkdir(class_dir)
-        try:
-            shutil.copyfile(dataset_root / dataset_prefix[args.dataset_name] / class_name / img_path.name, class_dir / img_path.name)
-        except:
-            raise ValueError(f"Image {class_name/img_path.name} not found in {dataset_root / dataset_prefix[args.dataset_name]}")
+        if args.dataset_name == "SUN397":
+            prefix = class_name[0]
+            shutil.copyfile(dataset_root / dataset_prefix[args.dataset_name] / prefix / class_name / img_path.name, class_dir / img_path.name)
+        else:
+            try:
+                shutil.copyfile(dataset_root / dataset_prefix[args.dataset_name] / class_name / img_path.name, class_dir / img_path.name)
+            except:
+                shutil.copyfile(dataset_root / dataset_prefix[args.dataset_name] / img_path.name, class_dir / img_path.name)
